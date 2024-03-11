e2aae22e4c0dd16164c9753235bb7c0f63e85c8c
Steven Chen <s.chen@databricks.com>
2024-03-11 00:53:28 -0700

    format

--- src/mistral_tokenize.py
+++ src/mistral_tokenize.py
@@ -3,5 +3,7 @@ from transformers import AutoTokenizer
 TEST_SENTENCE = "Tested sentence to encode."
 tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-v0.1")
 
-print(tokenizer.tokenize(TEST_SENTENCE))
+tokens = tokenizer.tokenize(TEST_SENTENCE)
+
+print(" ".join(tokens))
 print(tokenizer(TEST_SENTENCE))
